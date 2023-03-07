3542d5ef3fc9ba7cd96505f7810fec340be25235
Stefan Baggoo <itsrhyett@gmail.com>
2023-03-06 20:19:28 -0400

    Redid code layout and website layout

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-const signupButton = document.getElementById("signup");
-const loginButton = document.getElementById("login");
-const startUpContainer = document.getElementById("startup_container");
-const signUpContainer = document.getElementById("signup_option");
-const loginContainer = document.getElementById("login_option");
-const cancelButton = document.getElementsByClassName("cancel_button");
+const loginButton = document.getElementById("login_button");
+const signupButton = document.getElementById("signup_button");
+const signupOption = document.getElementById("signup_option");
+const loginOption = document.getElementById("login_option");
+const startupContainter = document.getElementById("button_container");
 
 
-signupButton.addEventListener("click", () => {
-    startUpContainer.style.display="none";
-    signUpContainer.style.display="flex";
-});
+function hideStartupContainer(){
+    startupContainter.style.display = "none";
+}
+
+function showLoginOption(){
+    loginOption.style.display = "block";
+}
+
+function showSignupOption(){
+    signupOption.style.display = "block";
+}
 
 loginButton.addEventListener("click", () => {
-    startUpContainer.style.display="none";
-    loginContainer.style.display="flex";
+    hideStartupContainer();
+    showLoginOption();
 });
 
-cancelButton.addEventListener("click", () => {
-    startUpContainer.style.display="grid";
-    signUpContainer.style.display="none";
-    loginContainer.style.display="none";
+signupButton.addEventListener("click", () => {
+    hideStartupContainer();
+    showSignupOption();
 });
